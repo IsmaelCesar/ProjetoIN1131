074f235b4af933f2cf90929cca89026ff2fd81a1
IsmaelCesar <leamscesar@gmail.com>
2023-12-04 09:32:40 -0300

    displaying plots of the route

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from selection_operators import Parent_Selection, Elitism
 from population import Get_Predefined_Data, Initialization, Fitness_Calculation
 import statistics
 import logging
+import matplotlib.pyplot as plt
 
 logger = logging.getLogger("tsp_ga")
 
@@ -39,6 +40,20 @@ def main():
             aux.append(((X_Coord[i] - X_Coord[k])**2 +(Y_Coord[i] - Y_Coord[k])**2)**1/2)
         Dist.append(aux)
 
+    
+    X = X_Coord
+    Y = Y_Coord
+
+    fig = plt.figure(figsize=(6,6))
+    ax = fig.add_subplot(1, 1, 1, aspect=1)
+    for i in range(len(Cidades)):
+        ax.text(X_Coord[i], Y_Coord[i], Cidades_Codigo[i], fontsize=6, fontweight='bold', color='black', ha='center', va='center')
+
+    plt.scatter(X, Y, s=20, c=None)
+    plt.title("Travelling Salesman Problem", fontsize=14, fontweight='bold')
+    plt.xlabel('X_Coord')
+    plt.ylabel('Y_Coord')
+    plt.show()
 
     """**RESOLUÇÃO DO PROBLEMA EM UMA ÚNICA RODADA**"""
 
@@ -75,9 +90,42 @@ def main():
         Sorted_Population = np.array(sorted(Fitness, reverse=True, key=lambda x:x[-1]))
         logger.info('Generation {}, Best_Overall_Solution {}'.format(major_it+1,Best_Overall[-1]))
 
-        Plot_Average = [-Average_of_Generation[i] for i in range(len(Average_of_Generation))]
-        Plot_Best = [-Best_of_Generation[i][-1] for i in range(len(Best_of_Generation))]
+        #Plot_Average = [-Average_of_Generation[i] for i in range(len(Average_of_Generation))]
+        #Plot_Best = [-Best_of_Generation[i][-1] for i in range(len(Best_of_Generation))]
     
+    Plot_Average = [-Average_of_Generation[i] for i in range(len(Average_of_Generation))]
+    Plot_Best = [-Best_of_Generation[i][-1] for i in range(len(Best_of_Generation))]
+
+    plt.plot(Plot_Average, color='gray')
+    plt.plot(Plot_Best, color='blue')
+    plt.xlabel('Generations')
+    plt.ylabel('Objective_Function')
+    plt.show()
+
+    Plot_Cycle = [Best_Overall[i] for i in range(len(Best_Overall) -1)]
+    Plot_X, Plot_Y, Plot_Codigos = [], [], []
+    for i in Plot_Cycle:
+        Plot_X.append(X_Coord[int(i)])
+        Plot_Y.append(Y_Coord[int(i)])
+        Plot_Codigos.append(Cidades_Codigo[int(i)])
+
+    Plot_X.append(Plot_X[0])
+    Plot_Y.append(Plot_Y[0])
+    Plot_Codigos.append(Plot_Codigos[0])
+    Plot_Cycle.append(Plot_Cycle[0])
+
+    fig = plt.figure(figsize=(8,20))
+    ax = fig.add_subplot(1, 1, 1, aspect=1)
+    for i in range(len(Plot_Cycle)):
+        ax.text(Plot_X[i], Plot_Y[i], Plot_Codigos[i], fontsize=6, fontweight='bold', color='black', ha='center', va='center')
+
+    plt.scatter(Plot_X, Plot_Y, s=20, c=None)
+    plt.plot(Plot_X, Plot_Y)
+    plt.title("Travelling Salesman Problem Optimized", fontsize=14, fontweight='bold')
+    plt.xlabel('X_Coord')
+    plt.ylabel('Y_Coord')
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
