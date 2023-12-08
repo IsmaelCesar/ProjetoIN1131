a038b9fb951a07bb1a46763efc589d6bb1bd3444
IsmaelCesar <leamscesar@gmail.com>
2023-12-08 17:56:02 -0300

    computing fitness more efficiently

--- operations/fitness.py
+++ operations/fitness.py
@@ -1,3 +1,21 @@
 import numpy as np
 
 
+
+def distance_fitness(individual: np.ndarray, distance_matrix: np.ndarray) -> float:
+    """
+    Computes the fitness of a single individual according to the distance matrix
+    """
+
+    point_a_range = range(0, len(individual) - 1)
+    point_b_range = range(1, len(individual))
+    individual_fitness = 0.
+    for point_a, point_b in zip(point_a_range, point_b_range):
+        # extract an edge from a route
+        individual_pos1 = individual[point_a]
+        individual_pos2 = individual[point_b]
+        
+        # compute fitness
+        individual_fitness += distance_matrix[individual_pos1][individual_pos2]
+
+    return individual_fitness
