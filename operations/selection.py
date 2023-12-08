89cdf9317215cec713de4c923a26e29c6d29e2f3
IsmaelCesar <leamscesar@gmail.com>
2023-12-08 15:16:11 -0300

    roulette wheel algorithm

--- operations/selection.py
+++ operations/selection.py
@@ -23,10 +23,10 @@ class SelectParents:
         Select individuals from individuals array for reproduction based on tournament algorithm
         """
         parent_idx = 0
-        parents = [None, None]
+        parents = [None] * self.num_parents
         pool_range = list(range(len(individuals)))
 
-        while parent_idx < self.num_parents:
+        while parent_idx < len(parents):
             individuals_indices = np.random.choice(pool_range, 3)
             best_one = individuals_indices[0]
             for indiv_idx in individuals_indices[1:]:
@@ -35,6 +35,37 @@ class SelectParents:
                     pool_range.remove(indiv_idx)
             
             parents[parent_idx] = individuals[best_one]
+            parent_idx += 1
+        
+        return parents
+
+    def roulette_wheel(
+            self, 
+            individuals: np.ndarray, 
+            fitness: np.ndarray,
+            scale_fitness: bool = True) -> List[np.ndarray, np.ndarray]:
+        """
+        Applies roulette wheel algorithm for selecting parents.
+        """
+
+        parents = [None] * self.num_parents
+        parent_idx = 0
+        pool_range = list(range(len(individuals)))
+
+        if scale_fitness:
+            # scales fitness values to they all sum up to one
+            temp_fit = fitness / fitness.sum()
+        else: 
+            temp_fit = scale_fitness
+
+        while parent_idx < len(parents):
+
+            random_prob = np.random.uniform(low= 0., high=1 / len(individuals))
+            indiv_count = 0 
+            while temp_fit[pool_range[indiv_count]] < random_prob: 
+                indiv_count += 1
+
+            parents[parent_idx]
             parent_idx += 1
         
         return parents
