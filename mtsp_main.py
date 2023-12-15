ee60710ce5d3fba97004fda962545d94ac640a0f
IsmaelCesar <leamscesar@gmail.com>
2023-12-15 16:43:19 -0300

    experiments configurations

--- mtsp_main.py
+++ mtsp_main.py
@@ -10,7 +10,7 @@ from operations.fitness import MinSumFitnessCalculator, MinMaxFitnessCalculator,
 from population import get_data_cidades
 from plotting import plot_cities, plot_mtsp_cycles, plot_objective_function
 from utils import save_statistics_as_json, compute_traveler_breaks, check_create_dir
-from typing import List
+from typing import List, Tuple
 from scipy.spatial.distance import cdist
 
 
@@ -29,9 +29,10 @@ def min_sum(
         n_gen: int,
         pop_size: int,
         execution_index: int,
+        op_probabilities: Tuple[float, float] = (.8, .2),
         combine_operations: bool = True,
-        results_dir: str = "results",
-        method_name: str = "min_sum"):
+        method_name: str = "min_sum",
+        results_dir: str = "results"):
     
     # handling directories <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     method_dir = os.path.join(results_dir, method_name)
@@ -52,8 +53,8 @@ def min_sum(
     pop_size = pop_size
     mtsp.evolve(
         pop_initializer=Initialization(num_cidades=len(cidades_id), pop_size=pop_size, origin=random_origin),
-        crossover_op=SingleTravelerX(crossover_type="order", probability=.8),
-        mutation_op=SingleTravelerMut(mutation_type="scramble", probability=.2),
+        crossover_op=SingleTravelerX(crossover_type="order", probability=op_probabilities[0]),
+        mutation_op=SingleTravelerMut(mutation_type="scramble", probability=op_probabilities[1]),
         selection_op=SelectIndividuals(),
         fitness_calculator=MinSumFitnessCalculator(distance_matrix),
         #survivor_selection= STSPKElitism()#FitnessProportional(pop_size=pop_size, num_cidades=len(escolas_id) - 1)
@@ -99,9 +100,10 @@ def min_max(
         n_gen: int,
         pop_size: int,
         execution_index: int,
+        op_probabilities: Tuple[float, float] = (.8, .2),
         combine_operations: bool = True,
-        results_dir: str = "results",
-        method_name: str = "min_max"):
+        method_name: str = "min_max",
+        results_dir: str = "results"):
     
     # handling directories <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     method_dir = os.path.join(results_dir, method_name)
@@ -122,8 +124,8 @@ def min_max(
     pop_size = pop_size
     mtsp.evolve(
         pop_initializer=Initialization(num_cidades=len(cidades_id), pop_size=pop_size, origin=random_origin),
-        crossover_op=SingleTravelerX(crossover_type="edge", probability=.8),
-        mutation_op=SingleTravelerMut(mutation_type="insert", probability=.3),
+        crossover_op=SingleTravelerX(crossover_type="edge", probability=op_probabilities[0]),
+        mutation_op=SingleTravelerMut(mutation_type="insert", probability=op_probabilities[1]),
         selection_op=SelectIndividuals(),
         fitness_calculator=MinMaxFitnessCalculator(distance_matrix),
         #survivor_selection= STSPKElitism()#FitnessProportional(pop_size=pop_size, num_cidades=len(escolas_id) - 1)
@@ -167,28 +169,54 @@ if __name__ == "__main__":
 
     RESULTS_DIR  = "results/"
 
+    # configuring logging <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    main_logger = logging.getLogger("mtsp_main")
+    main_logger.setLevel(logging.INFO)
+
     file_handler = logging.FileHandler(f"{RESULTS_DIR}/experiment.log", mode="w+")
-    logger.addHandler(file_handler)
+    stream_handler = logging.StreamHandler()
+    
+    main_logger.addHandler(file_handler)
+    main_logger.addHandler(stream_handler)
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
     cidades_codigo, cidades_id, coordenadas_cidades = get_data_cidades()
 
     traveler_breaks = compute_traveler_breaks(3, len(cidades_id))
 
-    logger.info(f"Number of cities: {len(cidades_id)}")
-    logger.info(f"Traveler breaks: {traveler_breaks}")
+    main_logger.info(f"Number of cities: {len(cidades_id)}")
+    main_logger.info(f"Traveler breaks: {traveler_breaks}")
 
-    #min_sum(
-    #    traveler_breaks=traveler_breaks, 
-    #    cidades_id=cidades_id, 
-    #    coordenadas_cidades=coordenadas_cidades, 
-    #    n_gen=1000,
-    #    pop_size=10,
-    #    execution_index=1)
-    
-    min_max(
-        traveler_breaks=traveler_breaks, 
-        cidades_id=cidades_id, 
-        coordenadas_cidades=coordenadas_cidades, 
-        n_gen=1000,
-        pop_size=10,
-        execution_index=1)
+    n_gen = 1000
+    pop_size = 10
+    combine_operations = True
+    op_probabilities = (.7, .3) # probabilitys of crossover and mutation to occur
+
+    main_logger.info(f"Total Generations: {n_gen}")
+    main_logger.info(f"Population size: {pop_size}")
+
+    n_executions = 30
+
+    for exec_idx in range(n_executions):
+        main_logger.info(f"Running Min-Sum Execution {exec_idx}")
+        min_sum(
+            traveler_breaks=traveler_breaks,
+            cidades_id=cidades_id,
+            coordenadas_cidades=coordenadas_cidades,
+            n_gen=n_gen,
+            pop_size=pop_size,
+            execution_index=exec_idx,
+            op_probabilities=op_probabilities,
+            combine_operations=combine_operations)
+
+        main_logger.info(f"Running Min-Max Execution {exec_idx}")
+
+        min_max(
+            traveler_breaks=traveler_breaks,
+            cidades_id=cidades_id,
+            coordenadas_cidades=coordenadas_cidades,
+            n_gen=n_gen,
+            pop_size=pop_size,
+            execution_index=exec_idx,
+            op_probabilities=op_probabilities,
+            combine_operations=combine_operations)
